7223d9468502a26f9ea69b61279da71e3fa1558a
Novarc <2124363741@qq.com>
2024-05-09 12:07:31 +0800

    start

--- include/argparse.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <type_traits>
-namespace viole {
-template <typename T>
-concept Enum = std::is_enum<T>::value;
-}
---
+++ src/util.ixx
@@ -0,0 +1,2 @@
+export module viole:util;
+import <string>;
