714afeaefbd114c84ac631278c1c702dae8b9c57
Novarc <2124363741@qq.com>
2024-06-03 22:41:21 +0800

    fiz

--- include/viole/systems/argparser.hpp
+++ include/viole/systems/argparser.hpp
@@ -13,7 +13,7 @@ protected:
 
 public:
   virtual void parse(basic_input &input) = 0;
-  [[nodiscard]] virtual std::string information() const noexcept;
+  virtual void information(basic_output &output) const noexcept;
   void to_string(basic_output &output) const noexcept override;
   [[nodiscard]] const std::type_info &get_type() const noexcept override;
   [[nodiscard]] std::string to_string() const noexcept override;
@@ -21,13 +21,26 @@ public:
 template <typename T> class basic_type_arg : public basic_arg {
 protected:
   T *source;
+  std::string arg_name;
 
 public:
-  void parse(basic_input &input) override;
-  basic_type_arg(std::string _name, std::string _description, T *source) {
+  void parse(basic_input &input) override = 0;
+  basic_type_arg(std::string _name, std::string _description,
+                 std::string arg_name, T *source) {
     name = _name;
     description = _description;
     this->source = source;
+    this->arg_name = arg_name;
+  }
+  void information(basic_output &output) const noexcept override {
+    output.write_char('[');
+    output.write_string(arg_name);
+    output.write_char(':');
+    output.write_string(typeid(T).name());
+    output.write_char(']');
+  }
+  [[nodiscard]] const std::type_info &get_type() const noexcept override {
+    return typeid(basic_type_arg<T>);
   }
 };
 /**
