4de0722568002c1dc135cc0b17b84f9441a1177e
Novarc <13143105+novarc@user.noreply.gitee.com>
2024-06-23 14:38:39 +0800

    plugin

--- test/plugin/main.cpp
+++ test/plugin/main.cpp
@@ -4,14 +4,12 @@ class foo {
   [[viole::must_override]] virtual void test2();
 };
 class bar : public foo {
-  [[viole::must_override_del]] void test1() override;
+  void test1() override;
   void test2() override;
 };
-class bar2 : public bar {
-  void test2() override;
-};
+class bar2 : public bar {};
 auto main() -> int {
-  
+
   std::cout << "test plugin" << std::endl;
   return 0;
 }
